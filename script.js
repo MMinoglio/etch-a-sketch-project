34c3dad2e5d098b691bf38376d3efaa77c153cd8
MMinoglio <martin.minoglio@outlook.com>
2021-12-27 20:57:04 -0300

    lista grilla responsiva y boton de reset en html y css

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-const contanier = document.querySelector('.container');
+function createGrid (size) {
 
-for (i = 0; i<256; i++) {
-  const div = document.createElement('div');
-  div.classList.add(`div-${i}`);
-  div.classList.add('divs');
-  contanier.appendChild(div);
+  const container = document.querySelector('.container');
+  for (x = 0; x<size; x++) {
+    const divx = document.createElement('div');
+    divx.classList.add(`divx${x}`)
+    divx.classList.add('divsx')
+    for (y = 0; y < size; y++) {
+      const divy = document.createElement('div');
+      divy.classList.add(`divy${y}`);
+      divy.classList.add('divsy');
+      divx.appendChild(divy);
+    }
+    container.appendChild(divx);
+  }
 }
+
+function changeSquareColorOnHover () {
+  const divsy = document.querySelectorAll('.divsy');
+  divsy.forEach(div => {
+    div.addEventListener("mouseenter", function(event){
+      event.target.style.backgroundColor = "purple";
+    }) 
+  });
+}
+
+createGrid(16);
+changeSquareColorOnHover ()
